fd88c626be97e4b45346ee5048da86d6b6de1d27
fhaedo3 <135553897+fhaedo3@users.noreply.github.com>
2024-05-02 19:34:32 -0300

    ajustes

---
+++ Ecommerce Api/src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+
+
+function NavBar (){ //le paso como prop la tarea para renderizar las tareas que vamos a escribir en el form
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default NavBar
